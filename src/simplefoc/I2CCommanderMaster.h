06d72be82fb3aa548b8dc49fdf9ccf13f2c0b87a
Richard Unger <runger1@gmail.com>
2024-04-28 21:22:41 +0200

    add motor status enum to I2CCommander header

--- src/simplefoc/I2CCommanderMaster.h
+++ src/simplefoc/I2CCommanderMaster.h
@@ -13,6 +13,18 @@
 
 namespace simplefoc {
 
+    enum FOCMotorStatus : uint8_t {
+        motor_uninitialized = 0x00,     //!< Motor is not yet initialized
+        motor_initializing  = 0x01,     //!< Motor intiialization is in progress
+        motor_uncalibrated  = 0x02,     //!< Motor is initialized, but not calibrated (open loop possible)
+        motor_calibrating   = 0x03,     //!< Motor calibration in progress
+        motor_ready         = 0x04,     //!< Motor is initialized and calibrated (closed loop possible)
+        motor_error         = 0x08,     //!< Motor is in error state (recoverable, e.g. overcurrent protection active)
+        motor_calib_failed  = 0x0E,     //!< Motor calibration failed (possibly recoverable)
+        motor_init_failed   = 0x0F,     //!< Motor initialization failed (not recoverable)
+    };
+
+
     typedef struct {
         TwoWire* wire;
         uint8_t address;    

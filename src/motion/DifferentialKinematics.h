d014ed3312efeb748f5f16f752e2f0b37ff3f29f
Richard Unger <runger1@gmail.com>
2024-04-29 16:06:54 +0200

    add comment

--- src/motion/DifferentialKinematics.h
+++ src/motion/DifferentialKinematics.h
@@ -31,10 +31,20 @@ namespace SmallRobots {
             DifferentialPathPlanner(DifferentialKinematics& kinematics);
             ~DifferentialPathPlanner();
 
-            virtual void move(float distance, float speed, float radius = RADIUS_STREIGHT);
-            virtual void rotate(float angle, float speed);
+            // TODO think  about where these methods belong
+            // possible model to seperate the concepts
+            //  - Kinematics - models the robot dimensions, type of drive etc, 
+            //                 thus implementing raw motion e.g. like DifferentialKinematics
+            //  - PathPlanner - composes paths in terms of the robot's capabilities, e.g. Point 
+            //                  and Shoot, decomposing curves into circular arcs, etc.
+            //  - "MotionController"? - executes paths in terms of raw movements over time/distance.
+            //                          "DeadReconningMotionController", or "OdometricMotionController"
+            //                          could be examples. Keeps state in terms of motions in progress,
+            //                          and provides facilities to queue and/or interrupt motions.
+            virtual void move(float distance, float speed, float radius = RADIUS_STREIGHT) = 0;
+            virtual void rotate(float angle, float speed) = 0;
 
-            void moveTo(float dx, float dy, float da, float speed);
+            virtual void moveTo(float dx, float dy, float da, float speed) = 0;
 
             DifferentialKinematics& kinematics;
     };

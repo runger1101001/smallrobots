4f2f2c29969bdd6ae84646dff15847c2eb42ae3a
runger1101001 <runger1@gmail.com>
2025-04-05 00:29:10 +0200

    telemetry updates

--- src/esp32/wifi/MQTTTelemetry.h
+++ src/esp32/wifi/MQTTTelemetry.h
@@ -21,24 +21,54 @@ namespace SmallRobots {
         };
         
         void sendTelemetry(uint8_t* data, size_t len, const char* subtopic = nullptr) {
-            if (mqtt.connected()) {
-                String full_topic = String(topic) + '/' + _hostname; // TODO make this more efficient!
-                if (subtopic)
-                    full_topic += String("/") + subtopic;
-                //smallrobots_debug.printf("MQTT publish %s size %d\n", full_topic.c_str(), len);
-                mqtt.publish(full_topic.c_str(), data, len);
-            }
-            else if (_hostname) {
-                if (mqtt.connect(_hostname))
-                    smallrobots_debug.println("MQTT reconnected");
-                else
-                    smallrobots_debug.println("MQTT not connected");
+            auto c = connect_and_topic(subtopic);
+            if (c.first) {
+                mqtt.publish(c.second.c_str(), data, len);
             }
         };
+
+
+        void sendTelemetry(size_t len, std::function<void(Print&)> data_provider, const char* subtopic = nullptr) {
+            auto c = connect_and_topic(subtopic);
+            if (c.first) {
+                String full_topic = c.second;
+                //smallrobots_debug.printf("MQTT publish %s size %d\n", full_topic.c_str(), len);
+                mqtt.beginPublish(full_topic.c_str(), len, false);
+                data_provider(mqtt);
+                mqtt.endPublish();
+            }
+        }
+
+
     protected:
         const char* _hostname = nullptr;
         PubSubClient& mqtt;
         const char* topic;
+
+        std::pair<bool, String> connect_and_topic(const char* subtopic = nullptr) {
+            bool connected = false;
+            if (!mqtt.connected()) {
+                if (_hostname) {
+                    if (mqtt.connect(_hostname)) {
+                        smallrobots_debug.println("MQTT reconnected");
+                        connected = true;
+                    }
+                    else
+                        smallrobots_debug.println("MQTT not connected");
+                }    
+            }
+            else
+                connected = true;
+            if (connected) {
+                String full_topic = String(topic) + '/' + _hostname; // TODO make this more efficient!
+                if (subtopic)
+                    full_topic += String("/") + subtopic;
+                return {connected, full_topic};
+            }
+            return {false, ""};
+        }
+
+
     };
 
 }; // namespace SmallRobots

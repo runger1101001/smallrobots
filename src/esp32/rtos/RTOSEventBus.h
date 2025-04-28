65f0d1afe57f5637832eb1ccf46dd95c99a445c0
runger1101001 <runger1@gmail.com>
2025-04-28 22:03:13 +0200

    more kinds of dispatchers

--- src/esp32/rtos/RTOSEventBus.h
+++ src/esp32/rtos/RTOSEventBus.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "../../control/SmallRobotEventBus.h"
+#include "./Lock.h"
 
 #if defined(ESP32)
 
@@ -32,6 +33,68 @@ namespace SmallRobots {
     protected:
         QueueHandle_t q;
     };
+
+
+    template<typename... Ts>
+    class RTOSTaskDispatcher {
+    public:
+        RTOSTaskDispatcher(TEventBus<RTOSTaskDispatcher, Ts...>& eventbus) : eventbus(eventbus) {
+            // TODO make core and stack-size configurable
+        };
+        ~RTOSTaskDispatcher() {
+        };
+        void queue(std::variant<Ts...>& event) {
+            std::tuple<std::variant<Ts...>, RTOSTaskDispatcher*> *tup = new std::tuple<>(event, this);
+            xTaskCreateUniversal(
+                [](void* arg) {
+                    std::tuple<std::variant<Ts...>, RTOSTaskDispatcher*> *tup = static_cast<std::tuple<std::variant<Ts...>, RTOSTaskDispatcher*>>(arg);
+                    tup[1]->eventbus.dispatchToListeners(tup[0]);
+                    delete tup;
+                    vTaskDelete(NULL);
+                },
+                "EventDispatcherTask",
+                1024,
+                this,
+                1,
+                NULL,
+                -1
+            );
+        };
+        int size() {
+            return 0;
+        };
+        std::variant<Ts...> next() {
+            throw std::runtime_error("No events in queue");
+        };
+    protected:
+        TEventBus<RTOSTaskDispatcher, Ts...>& eventbus;
+    };
+
+    const char LOCKEDQUEUEDISPATCHER_LOCKNAME[] = "LockedQueuedDispatcher";
+    typedef Lock<LOCKEDQUEUEDISPATCHER_LOCKNAME> LockedQueueDispatcherLock;
+
+    template<typename... Ts>
+    class LockedQueuedDispatcher {
+        public:
+            LockedQueuedDispatcher(TEventBus<LockedQueuedDispatcher, Ts...>& eventbus) {};
+            void queue(std::variant<Ts...> event) {
+                LockedQueueDispatcherLock lock;
+                q.push_back(event);
+            };
+            int size() {
+                LockedQueueDispatcherLock lock;
+                return q.size();
+            };
+            std::variant<Ts...> next() {
+                LockedQueueDispatcherLock lock;
+                auto event = q.front();
+                q.erase(q.begin());
+                return event;
+            };
+        protected:
+            std::vector<std::variant<Ts...>> q;
+    };
+
 
 };
 
